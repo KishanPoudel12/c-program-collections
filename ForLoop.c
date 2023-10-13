1675192ce3865f1e8341821a4af97bfd6f9a0660
kishan <kishanpoudel563@gmail.com>
2023-10-13 15:30:34 +0530

    Program to print numbers from smallest two digit(10) to greatest two digit number(99)

--- ForLoop.c
+++ ForLoop.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main(){
     int a;
-    for(a=1;a<=10;a++){
+    for(a=1;a<=10;a++){    //(initialization , condition , update/modify)  
         printf("My name is kishan poudel\n");
 
     }
