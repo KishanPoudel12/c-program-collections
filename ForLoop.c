e95e5b5f1cfb0c8b88aa1bd6b33d992014bab584
kishan <kishanpoudel563@gmail.com>
2023-10-13 14:51:14 +0530

    Program to print any  sentence Nth  times

--- ForLoop.c
+++ ForLoop.c
@@ -1,7 +1,8 @@
+// Program to print a sentence TEN times 
 #include<stdio.h>
 int main(){
     int a;
-    for(a=1;a<=100000;a++){
+    for(a=1;a<=10;a++){
         printf("My name is kishan poudel\n");
 
     }
