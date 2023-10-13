0edf388d6df370f885fd88ea9d60df67d05588ca
kishan <kishanpoudel563@gmail.com>
2023-10-13 20:27:37 +0530

    Incerment & Decrement sideways in for loop

---
+++ ForLoop3.c
@@ -0,0 +1,11 @@
+// Incerment & Decrement sideways in for loop
+#include<stdio.h>
+int main(){
+    int a,b;
+    for(a=10,b=0;a<=10,b<=10;a--,b++){   //(initialization , condition , update/modify)
+        printf("a=%d b=%d\n",a,b);
+
+    }
+    return 0;
+
+} 
