32df9f0712c1f56817d57652342529fdc071b233
kishan <kishanpoudel563@gmail.com>
2023-10-13 20:45:34 +0530

    Incerment & Decrement sideways in For  loop

---
+++ ForLoop1.c
@@ -0,0 +1,10 @@
+// Program to print a sentence TEN times 
+#include<stdio.h>
+int main(){
+    int a;
+    for(a=1;a<=10;a++){      //(initialization , condition , update/modify)
+        printf("My name is kishan poudel\n");
+
+    }
+    return 0;
+}
