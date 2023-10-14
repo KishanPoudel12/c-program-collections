6733708887e9936ea24b432c0bd851814158179a
kishan <kishanpoudel563@gmail.com>
2023-10-14 10:07:03 +0530

    Separated folder for  loops

---
+++ BitwiseOperationInForLoop.c
@@ -0,0 +1,10 @@
+// Bitwise Operation 
+#include<stdio.h>
+int main(){
+    int a,b;
+    for(a=100,b=0;a<=100 & b<=3;a--,b++){   //(initialization , condition , update/modify)
+        printf("a=%d b=%d\n",a,b);
+    }
+    return 0;
+
+} 
