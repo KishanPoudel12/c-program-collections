93d7ba85ffffdd3a245f1c27108ce5b8bdc9f177
kishan <kishanpoudel563@gmail.com>
2023-10-18 22:24:02 +0530

    while loop for printing a sentences many times

---
+++ loops/WhileLoop.c
@@ -0,0 +1,14 @@
+//  While loop for practise
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i < 10)
+
+    {
+        printf("My name is kishan poudel\n");
+
+        i++;
+    }
+    return 0;
+}
