27ca3b59b69358d66e294106fed11aa5fa41fd89
kishan <kishanpoudel563@gmail.com>
2023-10-18 22:39:08 +0530

    modified some changes

--- Fuctions/Function2.c
+++ Fuctions/Function2.c
@@ -8,13 +8,4 @@ int main(){
     return 0;
 }
 
-// #include<stdio.h>
-// int sum(int a,int b){
-//     return a+b;  //Question
-// }
-// int main(){
-// int add= sum(10,56);
-// printf("The sum is %d",add);
-// return 0; 
-// }
 
--- loops/ForLoop1.c
+++ loops/ForLoop1.c
@@ -1,10 +1,11 @@
-// Program to print any sentence many times 
-#include<stdio.h>
-int main(){
+// Program to print any sentence many times
+#include <stdio.h>
+int main()
+{
     int a;
-    for(a=1;a<=10;a++){      //(initialization , condition , update/modify)
+    for (a = 1; a <= 10; a++)
+    { //(initialization , condition , update/modify)
         printf("My name is kishan poudel\n");
-
     }
     return 0;
 }
