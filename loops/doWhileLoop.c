790a8de9ec89dbd6e2e12bffb5e87bd58bd395d3
kishan <kishanpoudel563@gmail.com>
2023-10-19 12:44:31 +0530

    do while loop model

---
+++ loops/doWhileLoop.c
@@ -0,0 +1,11 @@
+// do while loop model
+#include<stdio.h>
+int main(){
+    int a=0;
+    do{
+        printf("My name is kishan poudel\n");
+        a++;
+    }
+    while(a<=8);    //check once using a<0
+    return 0;
+}
