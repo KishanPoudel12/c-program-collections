dec8cd1e1e9a49578d548ba2d84241791323c2ad
kishan <kishanpoudel563@gmail.com>
2023-12-11 09:59:15 +0530

    Paper Size problem

--- Let_Us_C_book.c/problem5.c
+++ Let_Us_C_book.c/problem5.c
@@ -3,10 +3,19 @@ subsequent size A(n) is defined as A(n-1) cut in half parallel to its
 shorter sides. Thus paper of size A1 would have dimensions 841
 mm x 594 mm. Write a program to calculate and print paper sizes
 A0, A1, A2, … A8*/
- 
-#include<stdio.h>
-int main(){
 
+#include <stdio.h>
+int main()
+{
+    float l = 1189, b = 841, s;
+    int i;
 
-return 0;
+    for (i = 1; i <= 8; i++)
+    {
+      printf("\nA%d : l=%f ,b=%f\n",i,l,b);
+      s=l;       // s=1189  l=1189
+      l=b;       // l=841  b=841
+      b=s/2;     // b=1189/2  
+    }
+    return 0;
 }
