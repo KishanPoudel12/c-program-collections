214aba028811dcc208828122a947d04ba8707e11
kishan <kishanpoudel563@gmail.com>
2024-01-19 21:33:51 +0530

    let us c books problems

---
+++ Let_Us_C_book.c/gets_puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char s1[10];
+    char s2[10];
+    printf("enter the first word :");
+    gets(s1);
+    printf("enter the second word :");
+    gets(s2);
+    puts(s1);
+    puts(s2);
+    return 0;
+}
