d909e6de0cc4e7103213c0e631ad16d926a994ec
kishan <kishanpoudel563@gmail.com>
2024-01-22 11:20:05 +0530

    let us c books problems

--- Let_Us_C_book.c/LeapYear.c
+++ Let_Us_C_book.c/LeapYear.c
@@ -13,14 +13,11 @@ int main(){
     int year;
     printf("Enter the year : ");
     scanf("%d",&year);
-    if(year%4==0){
+    if(    year%400==0  ||  year%100 !=0  &&   year%4==0){
         printf("it is a leap year");
-    }
-    else if (year%400==0){
-        printf("It is a leap year");
-    }
-    else if (year%100 !=0){
+         }
+    else {
         printf("It is not a leap year");
-    }
+         }
     return 0;
 }
